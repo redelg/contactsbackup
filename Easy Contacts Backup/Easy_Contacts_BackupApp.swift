37411161179dfe0a0efe743b58c576668d798681
Renzo Delgado <renzodelgado114@gmail.com>
2022-09-15 15:41:57 -0500

    Initial Commit

---
+++ Easy Contacts Backup/Easy_Contacts_BackupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_Contacts_BackupApp.swift
+//  Easy Contacts Backup
+//
+//  Created by Renzo Delgado on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Easy_Contacts_BackupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
